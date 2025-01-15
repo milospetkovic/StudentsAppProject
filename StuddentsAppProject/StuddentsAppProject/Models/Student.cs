6df2a214b2b2ccf49084b6a3edc375c58a03c673
DESKTOP-OKSJNAH\korisnik <mail2milos@yahoo.com>
2025-01-15 23:35:44 +0100

    adjusted list of students, trying different formats of student's birth date format - define output in model, in template and finally use a shared template to display all datetime values in the same format (refs #850)

--- StuddentsAppProject/StuddentsAppProject/Models/Student.cs
+++ StuddentsAppProject/StuddentsAppProject/Models/Student.cs
@@ -1,10 +1,19 @@
-﻿namespace StuddentsAppProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StuddentsAppProject.Models
 {
     public class Student
     {
         public int Id { get; set; }
+
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+        
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Date of birth")]
+        //[DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
         public DateTime BirthDate { get; set; }
     }
 }
