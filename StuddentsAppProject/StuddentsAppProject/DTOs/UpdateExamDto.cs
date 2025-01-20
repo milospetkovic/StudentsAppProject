e142138f0c00b905b7ebb30f7bd18a1e421d1fe4
milospetkovic <mail2milos@yahoo.com>
2025-01-20 19:51:58 +0100

    update exam dto extends create dto (refs #850)

--- StuddentsAppProject/StuddentsAppProject/DTOs/UpdateExamDto.cs
+++ StuddentsAppProject/StuddentsAppProject/DTOs/UpdateExamDto.cs
@@ -2,22 +2,9 @@
 
 namespace StuddentsAppProject.Models.DTOs
 {
-    public class UpdateExamDto
+    public class UpdateExamDto : CreateExamDto
     {
         [Required]
         public int Id { get; set; }
-
-        [Required]
-        public int StudentId { get; set; }
-
-        [Required]
-        public int SubjectId { get; set; }
-
-        [Required]
-        public DateTime DateOfExam { get; set; }
-
-        [Required]
-        [Range(5, 10)]
-        public int ExamMark { get; set; }
     }
 }
