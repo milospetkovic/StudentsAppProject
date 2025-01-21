610b76aaaf8bd9910d082f182447f878125e3093
milospetkovic <elitasoft.web@gmail.com>
2025-01-21 11:39:12 +0100

    wip templating - refs #860

--- StuddentsAppProject/StuddentsAppProject/Models/Student.cs
+++ StuddentsAppProject/StuddentsAppProject/Models/Student.cs
@@ -8,11 +8,11 @@ namespace StuddentsAppProject.Models
 
         [Display(Name = "First name")]
         [Required]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
         
         [Display(Name = "Last name")]
         [Required]
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
 
         [Display(Name = "Date of birth")]
         [Required]
