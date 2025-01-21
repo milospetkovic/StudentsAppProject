72a2d48143504aee9ebc1ede22e6978d98d33af5
milospetkovic <elitasoft.web@gmail.com>
2025-01-21 09:36:57 +0100

    added null and null forgiving definition to avoid compiler warnings (refs #850)

--- StuddentsAppProject/StuddentsAppProject/Data/AppDbContext.cs
+++ StuddentsAppProject/StuddentsAppProject/Data/AppDbContext.cs
@@ -7,11 +7,11 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<Student> Students { get; set; }
+    public DbSet<Student> Students { get; set; } = null!;
 
-    public DbSet<Subject> Subjects { get; set; }
+    public DbSet<Subject> Subjects { get; set; } = null!;
 
-    public DbSet<Exam> Exams { get; set; }
+    public DbSet<Exam> Exams { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- StuddentsAppProject/StuddentsAppProject/Models/Exam.cs
+++ StuddentsAppProject/StuddentsAppProject/Models/Exam.cs
@@ -9,12 +9,12 @@ namespace StuddentsAppProject.Models
         [Required]
         public int StudentId { get; set; }
                 
-        public Student Student { get; set; }
+        public Student? Student { get; set; }
 
         [Required]
         public int SubjectId { get; set; }
 
-        public Subject Subject { get; set; }
+        public Subject? Subject { get; set; }
 
         [Required]
         public DateTime DateOfExam { get; set; }
