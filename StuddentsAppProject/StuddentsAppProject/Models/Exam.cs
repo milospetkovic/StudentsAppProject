800dae9f22ec581e2fe46b5793d6a1a5452ac8a7
milospetkovic <elitasoft.web@gmail.com>
2025-01-25 21:34:44 +0100

    implemented CRUD for all 3 entities (refs #860)

--- StuddentsAppProject/StuddentsAppProject/Models/Exam.cs
+++ StuddentsAppProject/StuddentsAppProject/Models/Exam.cs
@@ -6,19 +6,23 @@ namespace StuddentsAppProject.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Student")]
         [Required]
         public int StudentId { get; set; }
                 
         public Student? Student { get; set; }
 
+        [Display(Name = "Subject")]
         [Required]
         public int SubjectId { get; set; }
 
         public Subject? Subject { get; set; }
 
+        [Display(Name = "Date of exam")]
         [Required]
         public DateTime DateOfExam { get; set; }
 
+        [Display(Name = "Mark")]
         [Required]
         [Range(5, 10)]
         public int ExamMark { get; set; }
